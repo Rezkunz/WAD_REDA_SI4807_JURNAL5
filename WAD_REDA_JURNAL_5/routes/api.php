83844a9a1cc85ceac7458474a374667503089481
udafadill <udafadill@gmail.com>
2025-12-06 15:42:39 +0700

    JURNAL MODUL 5

--- WAD_REDA_JURNAL_5/routes/api.php
+++ WAD_REDA_JURNAL_5/routes/api.php
@@ -15,10 +15,18 @@ use App\Http\Controllers\AuthController;
  * =============1================
  * unprotected routes for user registration and login
  */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 
+
+
+Route::apiResource('cassette', CassetteController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    //auth 
+    
     /**
      * ============2================
      * user logout route
